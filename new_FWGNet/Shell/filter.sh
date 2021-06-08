75843ddfd51d0f8f499d524f105c25c7c3b94b57
Carloslopes97 <lopes.carlosab@gmail.com>
2021-06-08 00:41:37 -0300

    Configuração Filtro Pós simulação OK

--- new_FWGNet/Shell/filter.sh
+++ new_FWGNet/Shell/filter.sh
@@ -2,20 +2,44 @@
 
 # Filter .pcap files 
 
-# Trace dir
+
 dir_tr_filter="/home/carl/New_Results/Filter_Traces/"
 
-# Por enquanto
-app_protocol="http"
-# Start Filter config
-sudo termshark -r ${dir_pcap_file} -T fields -E separator=/t -e ip.src -e ip.dst -e frame.time_relative -e frame.len -e frame.protocols -e tcp.len -e udp.length > ${dir_tr_filter}${app_protocol}_trace.txt
+if [[ $filter_pcap = "real" ]]
+then
+    # Trace dir
+    
+    # Por enquanto
+    app_protocol="http"
+    # Start Filter config
+    sudo termshark -r ${dir_pcap_file} -T fields -E separator=/t -e ip.src -e ip.dst -e frame.time_relative -e frame.len -e frame.protocols -e tcp.len -e udp.length > ${dir_tr_filter}${app_protocol}_trace.txt
 
 
-# Habilitando permissões ao arquivo gerado
-sudo chmod 777 ${dir_tr_filter}${app_protocol}_trace.txt
-# Preparando arquivo para ser legível como DataFrame
-sudo sed -i ':a;N;$!ba;s/\n/"\n"/g' "${dir_tr_filter}${app_protocol}_trace.txt"
-sudo sed -i ':a;N;$!ba;s/\t/";"/g' "${dir_tr_filter}${app_protocol}_trace.txt"
-# Adicionando aspas no começo e no final do arquivo
-sudo sed -i '1s/^/"/' "${dir_tr_filter}${app_protocol}_trace.txt"
-sudo sed -i '${s/$/"/}' "${dir_tr_filter}${app_protocol}_trace.txt"
+    # Habilitando permissões ao arquivo gerado
+    sudo chmod 777 ${dir_tr_filter}${app_protocol}_trace.txt
+    # Preparando arquivo para ser legível como DataFrame
+    sudo sed -i ':a;N;$!ba;s/\n/"\n"/g' "${dir_tr_filter}${app_protocol}_trace.txt"
+    sudo sed -i ':a;N;$!ba;s/\t/";"/g' "${dir_tr_filter}${app_protocol}_trace.txt"
+    # Adicionando aspas no começo e no final do arquivo
+    sudo sed -i '1s/^/"/' "${dir_tr_filter}${app_protocol}_trace.txt"
+    sudo sed -i '${s/$/"/}' "${dir_tr_filter}${app_protocol}_trace.txt"
+fi
+
+if [[ $filter_pcap = "ns3" ]]
+then
+    # Trace dir
+  
+    # Por enquanto
+    app_protocol="http"
+    # Start Filter config
+    sudo termshark -r ${dir_pcap_file} -T fields -E separator=/t -e ip.src -e ip.dst -e frame.time_relative -e frame.len -e frame.protocols -e tcp.len -e udp.length > ${dir_tr_filter}ns3_${app_protocol}_trace.txt
+
+    # Habilitando permissões ao arquivo gerado
+    sudo chmod 777 ${dir_tr_filter}${app_protocol}_trace.txt
+    # Preparando arquivo para ser legível como DataFrame
+    sudo sed -i ':a;N;$!ba;s/\n/"\n"/g' "${dir_tr_filter}ns3_${app_protocol}_trace.txt"
+    sudo sed -i ':a;N;$!ba;s/\t/";"/g' "${dir_tr_filter}ns3_${app_protocol}_trace.txt"
+    # Adicionando aspas no começo e no final do arquivo
+    sudo sed -i '1s/^/"/' "${dir_tr_filter}ns3_${app_protocol}_trace.txt"
+    sudo sed -i '${s/$/"/}' "${dir_tr_filter}ns3_${app_protocol}_trace.txt"
+fi
